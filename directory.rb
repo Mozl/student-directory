a4897e926aabd2c1c1e9bc8396e623b592d97a8f
Louis Moselhi <moselhil@gmail.com>
2018-03-14 14:18:16 +0000

    added each with index & first name dependency

--- directory.rb
+++ directory.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    if student[:name].chr == "b"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
